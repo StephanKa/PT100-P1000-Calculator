d47145a29feeaa854ac6ebe101ef31c226f3d740
Stephan K <8549023+StephanKa@users.noreply.github.com>
2023-04-28 21:54:55 +0200

    fixed main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include <PTCalculator.h>
 #include <fmt/format.h>
 
+using namespace PTCalculator::unit_literals;
+
 int main()
 {
     fmt::print("Version: {}.{}-{}\n", Version::Major, Version::Minor, Version::Patch);
-    constexpr auto Resistance = 175.86F;
+    constexpr auto Resistance = 175.86_Ohm
     constexpr auto result = PTCalculator::calculate<PTCalculator::PT100>(Resistance);
-    fmt::print("Resistance: {} Ohm\nTemperature: {:.2f} °C\n", Resistance, result);
+    fmt::print("Resistance: {} Ohm\nTemperature: {:.2f} °C\n", Resistance(), result);
     return 0;
 }
