9df2be74842dc8c92cd818fe4f4f30aea24a0841
Stephan K <8549023+StephanKa@users.noreply.github.com>
2023-04-28 22:17:03 +0200

    fixed missing ;

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ using namespace PTCalculator::unit_literals;
 int main()
 {
     fmt::print("Version: {}.{}-{}\n", Version::Major, Version::Minor, Version::Patch);
-    constexpr auto Resistance = 175.86_Ohm
+    constexpr auto Resistance = 175.86_Ohm;
     constexpr auto result = PTCalculator::calculate<PTCalculator::PT100>(Resistance);
     fmt::print("Resistance: {} Ohm\nTemperature: {:.2f} °C\n", Resistance(), result);
     return 0;
